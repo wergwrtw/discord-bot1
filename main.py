07b5783c763de654d53d0b99b86f948606fc7218
wergwrtw <bicycle4514@icloud.com>
2025-07-18 14:31:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import discord
 from discord.ext import commands
 from keep_alive import keep_alive  # keep.py を別ファイルで作ってある前提
 
-
 # --- 環境変数からトークン読み込み ---
 TOKEN = os.environ["TOKEN"]
 
@@ -23,25 +22,24 @@ async def on_message(message):
     if message.author.bot:
         return
 
-   if message.channel.id == SELF_INTRO_CHANNEL_ID:
-    guild = message.guild
-    role = discord.utils.get(guild.roles, id=ROLE_ID)
-    if role:
-        # すでにロール持ってるか確認
-        if role not in message.author.roles:
-            await message.author.add_roles(role)
-            await message.channel.send(
-                f"{message.author.mention} さんにロール「{role.name}」を付与しました！\n"
-                "続けて、[最高ランク](https://discordapp.com/channels/1390595136093687901/1395760970630172742) で "
-                "自分の最高ランクのスタンプを押してください！"
-            )
+    if message.channel.id == SELF_INTRO_CHANNEL_ID:
+        guild = message.guild
+        role = discord.utils.get(guild.roles, id=ROLE_ID)
+        if role:
+            # すでにロール持ってるか確認
+            if role not in message.author.roles:
+                await message.author.add_roles(role)
+                await message.channel.send(
+                    f"{message.author.mention} さんにロール「{role.name}」を付与しました！\n"
+                    "続けて、[最高ランク](https://discordapp.com/channels/1390595136093687901/1395760970630172742) で "
+                    "自分の最高ランクのスタンプを押してください！"
+                )
+            else:
+                print(f"{message.author} はすでにロールを持っています。")
         else:
-            print(f"{message.author} はすでにロールを持っています。")
-    else:
-        await message.channel.send("❌ ロールが見つかりませんでした。")
+            await message.channel.send("❌ ロールが見つかりませんでした。")
 
-await bot.process_commands(message)
-
+    await bot.process_commands(message)
 
 # --- 起動 ---
 keep_alive()
